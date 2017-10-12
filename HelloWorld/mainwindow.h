9b538ea37c19ef1a0b367eef310dfbb26b23c0be
qingchen58 <15201423238@163.com>
2017-10-13 00:56:44 +0800

    定义一个变量

--- HelloWorld/mainwindow.h
+++ HelloWorld/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +11,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    QLabel * qlabel;
 };
 
 #endif // MAINWINDOW_H
