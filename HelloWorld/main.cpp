8d187df6ac9da3c1f83883ed32265c4afd12c65b
qingchen58 <15201423238@163.com>
2017-10-13 01:13:12 +0800

    添加主屏幕定义

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,9 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QScreen>
+
+QScreen * g_screen = NULL;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    g_screen = a.primaryScreen();
+
     MainWindow w;
     w.show();
 
